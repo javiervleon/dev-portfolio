537ebc7279828b0b11d250c333e5aa1d1e7c806b
Javier Vásquez León <javiervleon99@gmail.com>
2025-11-20 16:36:48 -0300

    astro.config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://javiervleon.github.io",
+  site: "https://javiervleon.github.io/dev-portfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
